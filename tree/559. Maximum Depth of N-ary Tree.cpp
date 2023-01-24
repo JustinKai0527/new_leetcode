5b30899f1044e4392476b204c5d8486962777220
unknown <b10901187@g.ntu.edu.tw>
2023-01-24 22:17:05 +0800

    solution for 559

---
+++ tree/559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == NULL)  return 0;
+        int depth = 0;
+        for(auto it: root->children) {
+            depth = max(depth, maxDepth(it));
+        }
+
+        return depth + 1;
+    } 
+
+};

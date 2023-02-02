130f7dcfbe727a60b1cd5d514afa4f4c8d5ff8fa
unknown <b10901187@g.ntu.edu.tw>
2023-02-02 15:18:50 +0800

    complete

---
+++ tree/404. Sum of Left Leaves.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL && root->right== NULL) return 0;
+
+        int leftValue = sumOfLeftLeaves(root->left);    // 左
+        if (root->left && !root->left->left && !root->left->right) { // 左子树就是一个左叶子的情况
+            leftValue = root->left->val;
+        }
+        int rightValue = sumOfLeftLeaves(root->right);  // 右
+
+        int sum = leftValue + rightValue;               // 中
+        return sum;
+    }
+};

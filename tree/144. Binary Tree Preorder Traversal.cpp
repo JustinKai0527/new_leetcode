c7b250dec1bdfaa2237bda18323b899f9289cc9e
unknown <b10901187@g.ntu.edu.tw>
2023-02-02 13:42:55 +0800

    using stack to iteration

---
+++ tree/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include "tree.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;              //FILO
+        vector<int> vec;
+        if(root == NULL)  return vec;
+
+        st.push(root);
+        while(!st.empty()) {
+
+            TreeNode* tmp = st.top();
+            st.pop();
+            vec.push_back(tmp->val);
+            if(tmp->right != NULL)  st.push(tmp->right);
+            if(tmp->left != NULL)  st.push(tmp->left);
+        }
+
+        return vec;
+    }
+};

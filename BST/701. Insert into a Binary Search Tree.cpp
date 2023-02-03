219efc6d1c56f68d08905e96074863e66057f565
unknown <b10901187@g.ntu.edu.tw>
2023-02-03 15:40:50 +0800

    complete

---
+++ BST/701. Insert into a Binary Search Tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "tree.h"
+
+using namespace std;
+
+class Solution {
+public:
+
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+        if(root == NULL)  return new TreeNode(val);
+        if(root->val > val) {
+            root->left = insertIntoBST(root->left, val);
+        }
+
+        if(root->val < val) {
+            root->right = insertIntoBST(root->right, val);
+        }
+        
+        return root;
+    }
+};
